7118f619824e3f17667b39f5ce3689d966e33aab
sys1007 <venkatadri@way2news.com>
2023-05-18 15:11:05 +0530

    On Click Method Updated..

--- DemoLayout/src/main/java/com/dynamiclayout/demolayout/MainActivity.java
+++ DemoLayout/src/main/java/com/dynamiclayout/demolayout/MainActivity.java
@@ -1,14 +1,19 @@
 package com.dynamiclayout.demolayout;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.dynamiclayout.loader.DynamicException;
 import com.dynamiclayout.loader.DynamicListener;
 import com.dynamiclayout.loader.LayoutUpdater;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ViewGroup viewById;
@@ -21,9 +26,11 @@ public class MainActivity extends AppCompatActivity {
         InItViews();
         RequestData();
     }
-
+    private void InItViews() {
+        viewById = (ViewGroup) findViewById(R.id.mainLayout);
+    }
     private void RequestData() {
-        final String res = "https://raw.githubusercontent.com/vvsdevs/Demo/master/app/src/main/assets/";
+        final String res = "https://raw.githubusercontent.com/vvsdevs/AndroidDynamicLayoutLoader/master/DemoLayout/src/main/assets/";
         Log.d(TAG, "RequestData: ");
         try {
             new LayoutUpdater(res, "login_screen", viewById)
@@ -46,7 +53,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void InItViews() {
-        viewById = (ViewGroup) findViewById(R.id.mainLayout);
+
+    //Here onImageClick is onClick Method that we are mentioned in json
+    // parameters cannot be primitive types
+    public static void onImageClick(String name, Integer age, Context context) {
+        Toast.makeText(context, "Name: " + name + "\nAge: " + age, Toast.LENGTH_SHORT).show();
+    }
+
+    // first argument must be boolean and it will be used as 'checked' flag
+    public static void onButtonCheck(boolean isChecked, String message, Context context) {
+        Toast.makeText(context, "Checked: " + isChecked + "\nMessage: " + message, Toast.LENGTH_SHORT).show();
+
     }
 }
